877835b9484da07f71584414cfbd78f0252e294e
Андрей Добрин <dobrinandrei1992@gmail.com>
2023-09-17 07:31:38 +0600

    perf: Оптимизация типов
    - Исключен cjs
    - Удалены пакеты rollup-plugin-dts и @rollup/plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,25 @@
 import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import { dts } from 'rollup-plugin-dts';
 
 const packageJson = require('./package.json');
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      { file: packageJson.main, format: 'cjs' },
-      { file: packageJson.module, format: 'esm' },
-    ],
-    plugins: [
-      external(),
-      resolve(),
-      commonjs(),
-      babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
-      typescript({
-        tsconfig: './tsconfig.json',
-        exclude: ['node_modules', 'src/components/**/**.story.tsx'],
-      }),
-      terser(),
-    ],
-    onwarn(warning, warn) {
-      if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) {
-        return;
-      }
-      warn(warning);
-    },
+export default {
+  input: 'src/index.ts',
+  output: { file: packageJson.module, format: 'esm' },
+  plugins: [
+    external(),
+    resolve(),
+    babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
+    typescript(),
+    terser(),
+  ],
+  onwarn(warning, warn) {
+    if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) {
+      return;
+    }
+    warn(warning);
   },
-  {
-    input: 'dist/esm/index.d.ts',
-    output: [{ file: packageJson.types, format: 'esm' }],
-    plugins: [dts()],
-  },
-];
+};
