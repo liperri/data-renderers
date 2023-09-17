9432449af6d9f773a5e3ebccc4a67e491780a1ba
Андрей Добрин <dobrinandrei1992@gmail.com>
2023-09-17 06:15:31 +0600

    perf: Оптимизация пакета
    - Исключен react из сборки
    - Исключены sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import { dts } from 'rollup-plugin-dts';
@@ -11,20 +12,20 @@ export default [
   {
     input: 'src/index.ts',
     output: [
-      { file: packageJson.main, format: 'cjs', sourcemap: true },
-      { file: packageJson.module, format: 'esm', sourcemap: true },
+      { file: packageJson.main, format: 'cjs' },
+      { file: packageJson.module, format: 'esm' },
     ],
     plugins: [
+      external(),
       resolve(),
       commonjs(),
-      babel({ babelHelpers: 'bundled' }),
+      babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
       typescript({
         tsconfig: './tsconfig.json',
         exclude: ['node_modules', 'src/components/**/**.story.tsx'],
       }),
       terser(),
     ],
-    external: ['react', '@mui/material'],
     onwarn(warning, warn) {
       if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) {
         return;
