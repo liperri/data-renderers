6806de7eb274d213e81e1c2277f941dfea10e37f
Андрей Добрин <dobrinandrei1992@gmail.com>
2023-09-20 22:37:57 +0600

    refactor: Удален helper omit

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -9,22 +9,3 @@ export function getRandomNumber(min: number, max: number) {
 
   return number;
 }
-
-/**
- * omit
- * @param obj
- * @param keys
- * @returns
- */
-export function omit<TObj extends Record<string, any>, TKey extends keyof TObj>(
-  obj: TObj,
-  keys: TKey | TKey[],
-): Omit<TObj, TKey> {
-  if (!Array.isArray(keys)) keys = [keys];
-
-  const result: Partial<TObj> = {};
-
-  for (const key in obj) if (!keys.includes(key as unknown as TKey)) result[key as keyof TObj] = obj[key];
-
-  return result as Omit<TObj, TKey>;
-}
